184037a48819c6d584b65dc37ddf33883deddf97
aravind <aravindpatibandla@outlook.com>
2024-01-18 16:45:04 +0530

    maven commit

---
+++ src/main/java/com/demo/second_project/App.java
@@ -0,0 +1,13 @@
+package com.demo.second_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
